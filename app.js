a307640124d159a76462077db106ef50e70a897a
MaslovIgor <maslovigors@gmail.com>
2016-01-08 10:10:40 +0300

    move to express 4.x

--- app.js
+++ app.js
@@ -1,22 +1,18 @@
-var db = require ("./db");
-
 var express = require('express');
 var app = express();
+var db = require ("./db");
 
 function render(res, view)
 {
 	res.render(view, {ga_id : process.env.GAID || 'UA-00000000-0'});
 }
 
-app.configure(function() {
-    app.engine('html', require('uinexpress').__express);
-    app.set('view engine', 'html');
-    app.set('views', __dirname + "/tpl");
-	// static content for our site
-    app.use('/images', express.static(__dirname + "/images"));
-	app.use('/css', express.static(__dirname + "/css"));
-	app.use('/js', express.static(__dirname + "/js"));
-});
+app.engine('html', require('uinexpress').__express);
+app.set('view engine', 'html');
+app.set('views', __dirname + "/tpl");
+app.use('/images', express.static(__dirname + "/images"));
+app.use('/css', express.static(__dirname + "/css"));
+app.use('/js', express.static(__dirname + "/js"));
 
 app.get('/', function(req, res) {
     render(res, "index.html")
--- db.js
+++ db.js
@@ -1,4 +1,4 @@
-var mongoose = require ("mongoose");
+var mongoose = require("mongoose");
 var mongouri = 
   process.env.MONGOLAB_URI || 
   process.env.MONGOHQ_URL || 
